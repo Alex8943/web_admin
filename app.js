1e444a2f3026f00e35510541848becf0a2619966
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2024-02-24 16:05:25 +0100

    setup a good template for design

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express from "express"; 
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+app.use(express.static("public"));
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log("Server is running on port", port);
 }); 
